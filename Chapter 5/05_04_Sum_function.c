b9560bf4d27a3a9b5214a4088d0ba790a38b35d9
Sayan Mondal <sayanmondalg9@gmail.com>
2022-01-05 12:27:19 +0530

    rewrite

--- Chapter 5/05_04_Sum_function.c
+++ Chapter 5/05_04_Sum_function.c
@@ -8,12 +8,12 @@ int sum(int a, int b); //--> Function prototype + declaration.
 int main()
 {
     int c;
-    c = sum(5, 6); //--> Function call + intialize the value in 'c'.
+    c = sum(5, 6); //--> Function call + intialize the value in 'c'. these 5 and 6 is formal parameter
     printf("The value of c is %d\n", c);
     return 0;
 }
 
-int sum(int a, int b) //--> Function description
+int sum(int a, int b) //--> Function description these int a and int b is acctual parameter
 {
     int c;
     c = a + b;
